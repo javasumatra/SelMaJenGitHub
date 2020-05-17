06eb089cfdae9e4db2cc732cc926203a14360e4e
javasumatra <ravmenneni@gmail.com>
2020-05-16 21:27:16 -0500

    My first integration project

---
+++ TestIntegration/src/test/java/com/menneni/MortgageTests.java
@@ -0,0 +1,12 @@
+package com.menneni;
+//Adding comments to master
+import org.testng.annotations.Test;
+
+public class MortgageTests {
+
+	@Test
+	public void HomeMortgageTest(){
+		System.out.println("My Mortgage Account");
+	}
+	
+}
