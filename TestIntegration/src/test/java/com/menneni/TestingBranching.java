96f3d4a4637571e50c352b37736d3391750ba469
javasumatra <ravmenneni@gmail.com>
2020-05-17 19:43:04 -0500

    Created a new testd

---
+++ TestIntegration/src/test/java/com/menneni/TestingBranching.java
@@ -0,0 +1,19 @@
+package com.menneni;
+
+import org.testng.annotations.Test;
+
+public class TestingBranching {
+
+	@Test
+	public void HomeLoanCheck(){
+		System.out.println("My Mortgage Account");
+	}
+	@Test
+	public void CarLoanCheck(){
+		System.out.println("My CarLoan Account");
+	}
+	@Test
+	public void BusinessLoanCheck(){
+		System.out.println("My BusinessfLoan Account");
+	}
+}
