0acc0659ba2256b1ca51427773df4b278b30406b
javasumatra <ravmenneni@gmail.com>
2020-05-17 20:08:26 -0500

    Added a third test

---
+++ TestIntegration/src/test/java/com/menneni/CarLoanTests.java
@@ -0,0 +1,18 @@
+package com.menneni;
+
+import org.testng.annotations.Test;
+
+public class CarLoanTests {
+	@Test
+	public void HomeLoanCheck(){
+		System.out.println("My Mortgage Account");
+	}
+	@Test
+	public void CarLoanCheck(){
+		System.out.println("My CarLoan Account");
+	}
+	@Test
+	public void BusinessLoanCheck(){
+		System.out.println("My BusinessfLoan Account");
+	}
+}
